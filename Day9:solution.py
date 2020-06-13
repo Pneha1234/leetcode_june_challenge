8ce494b2d3fce27737223111e3494f024e039b8b
Pneha <neha.pandey@debajyotigroup.com>
2020-06-13 18:09:23 +0545

    day 8 and 9 solution

---
+++ Day9:solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_i, t_i = 0, 0
+        while s_i < len(s) and t_i < len(t):
+            s_i, t_i = s_i + (s[s_i] == t[t_i]), t_i + 1
+            
+        return s_i == len(s)
